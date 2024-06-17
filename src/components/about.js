8589fc360990cb0a2867db04ec564a33bd1af499
Iness Benaissa <ine.benss@gmail.com>
2024-06-17 16:27:26 +0200

    making some updates

--- src/components/about.js
+++ src/components/about.js
@@ -5,26 +5,24 @@ export function About() {
             <h3 className="about-title">En savoir plus sur Nesscake : </h3>
             <br />
             <br />
-            <div className="square">
-                <p className="about-text" > Je vous invite à entrer dans mon univers les Nesscake's, là
-                    où les recettes sont un délice !
-                    <br />
-                    Vous y trouverez des adresses à ne pas louper chaque semaines,
-                    des inspirations photos/vidéos de gâteaux, des astuces, et des recettes. &#128523;
-                    <br />
-                    Evidemment, <span className="underline">je les ai toutes écrites et ajustés pour qu'elles soient
-                        les meilleures (et les plus simples) que vous puissiez trouver. </span>  &#127874;
-                    <br />
-                    Ici, on croit fermement que la vie est trop courte pour résister à une bonne dose de sucre.
-                    Des cupcakes aux pancakes, en passant par des gâteaux dignes de ce nom.
-                    <br />
-                    Étudiante en informatique le jour, pâtissière du dimanche la nuit. J'ai développé ce site pour allier ces deux domaine pour lesquelles je porte de l'intérêt.
-                    <br />
-                    <span className="underline">Mes coordonnées sont en bas de page, n'hésitez pas si vous avez des questions ou des suggestions !</span>
-                    <br />
-                    Prêt.e.s à faire de grands bangers ? &#x1F469;&#x1F3FD;&#x200D;&#x1F373;
-                </p>
-            </div>
+            <p className="about-text" > Je vous invite à entrer dans mon univers les Nesscake's, là
+                où les recettes sont un délice !
+                <br />
+                Vous y trouverez des adresses à ne pas louper chaque semaines,
+                des inspirations photos/vidéos de gâteaux, des astuces, et des recettes. &#128523;
+                <br />
+                Evidemment, <span className="underline">je les ai toutes écrites et ajustés pour qu'elles soient
+                    les meilleures (et les plus simples) que vous puissiez trouver. </span>  &#127874;
+                <br />
+                Ici, on croit fermement que la vie est trop courte pour résister à une bonne dose de sucre.
+                Des cupcakes aux pancakes, en passant par des gâteaux dignes de ce nom.
+                <br />
+                Étudiante en informatique le jour, pâtissière du dimanche la nuit. J'ai développé ce site pour allier ces deux domaine pour lesquelles je porte de l'intérêt.
+                <br />
+                <span className="underline">Mes coordonnées sont en bas de page, n'hésitez pas si vous avez des questions ou des suggestions !</span>
+                <br />
+                Prêt.e.s à faire de grands bangers ? &#x1F469;&#x1F3FD;&#x200D;&#x1F373;
+            </p>
 
 
         </section>
