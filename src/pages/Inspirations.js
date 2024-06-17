a9658e1496693430d8c995295269431b67f8687b
Iness Benaissa <ine.benss@gmail.com>
2024-06-17 16:22:01 +0200

    making some updates

--- src/pages/Inspirations.js
+++ src/pages/Inspirations.js
@@ -4,8 +4,6 @@ import { Header } from '../components/header.js';
 import greenCookies from '../images/green-cookies.png';
 import pinacolaCake from '../images/pinacolacake.png';
 import spiderCake from '../images/spidercake.png';
-import box from '../images/box.png';
-import pinApple from '../images/pinapple.png';
 import heartCake from '../images/heart-cake.png';
 import floweredCake from '../images/flowered-cake.png';
 
@@ -19,8 +17,6 @@ function Inspirations() {
                 <img src={greenCookies} className="inspiration" alt="nesscake" />
                 <img src={pinacolaCake} className="inspiration" alt="nesscake" />
                 <img src={spiderCake} className="inspiration" alt="nesscake" />
-                <img src={box} className="inspiration" alt="nesscake" />
-                <img src={pinApple} className="inspiration" alt="nesscake" />
                 <img src={heartCake} className="inspiration" alt="nesscake" />
                 <img src={floweredCake} className="inspiration" alt="nesscake" />
             </div>
